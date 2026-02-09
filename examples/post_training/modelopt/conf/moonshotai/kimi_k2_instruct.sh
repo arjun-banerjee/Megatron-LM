195f2e075ca013dc8790d843bf09e42bcc09814a
Tejas Prabhune <prabhune@berkeley.edu>
2026-02-09 09:33:13 +0000

    Fix nemotron config

--- examples/post_training/modelopt/conf/moonshotai/kimi_k2_instruct.sh
+++ examples/post_training/modelopt/conf/moonshotai/kimi_k2_instruct.sh
@@ -3,6 +3,6 @@
 HF_MODEL_CKPT=/workspace/scratch/moonshotai/Kimi-K2-Instruct
 TP=1
 ETP=1
-EP=1 # Adjust EP to 1 when using PP to distribute layers across more GPUs.
-PP=8 # Introduce Pipeline Parallelism to split the 61 layers across 8 GPUs.
+EP=1
+PP=8
 
--- examples/post_training/modelopt/conf/nvidia/nemotron_eagle.sh
+++ examples/post_training/modelopt/conf/nvidia/nemotron_eagle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HF_MODEL_CKPT=/workspace/scratch/nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16
+HF_MODEL_CKPT=nvidia/NVIDIA-Nemotron-3-Nano-30B-A3B-BF16
 TP=1
 ETP=1
 EP=1
